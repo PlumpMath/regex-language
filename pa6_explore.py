82e3e4e7823604d7ff8c7858a94fe2d774a0bf0a
rickbhardwaj <rickbhardwaj@gmail.com>
2014-11-18 23:45:07 -0800

    added equivalence relations

--- pa6_explore.py
+++ pa6_explore.py
@@ -1,3 +1,91 @@
 #Exploring Python's generators for Project 6
 
-#Simplest generator examples
+###Simplest generator examples
+def simple_generator_function():
+	yield 1
+	yield 2
+	yield 3
+
+#Two ways to call generators
+for value in simple_generator_function():
+	print(value)
+
+#Should output:
+# 1
+# 2
+# 3
+
+our_generator = simple_generator_funciton()
+
+next(our_generator)
+#Should ouput:
+# 1
+
+next(our_generator)
+#Should ouput:
+# 2
+
+next(our_generator)
+#Should ouput:
+# 2
+
+
+###Generator pattern
+
+#This boilerplate code:
+
+class firstn(object):
+	def __init__(self, n):
+		self.n = n
+		self.num, self.nums = 0, []
+
+	def __iter__(self):
+		return self
+
+	#For Python 3 compatiability
+	def __next__(self):
+		return self.next()
+
+	def next(self):
+		if self.num < self.n:
+			cur, self.num = self.num, self.num+1
+			return cur
+		else:
+			raise StopIteration()
+
+#is equivalent to this Python idiom:
+
+def firstn(n):
+	num = 0
+	while num < n:
+		yield num
+		num += 1
+
+#Both can be called with:
+
+sum_of_first_n = sum(firstn(10000))
+
+###Cannonical examples:
+
+#We can rewrite this non-working solution:
+
+def get_primes(start):
+    for element in magical_infinite_range(start):
+        if is_prime(element):
+            return element
+
+def solve_number_10():
+    # She *is* working on Project Euler #10, I knew it!
+    total = 2
+    for next_prime in get_primes(3):
+        if next_prime < 2000000:
+            total += next_prime
+        else:
+            print(total)
+            return
+
+#with generator functions as follows:
+
+def get_primes(number):
+	while True:
+		if is_prime(number)
