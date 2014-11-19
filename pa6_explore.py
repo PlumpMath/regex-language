420464b73750fa934bc2023e32fb7e8b50261816
rickbhardwaj <rickbhardwaj@gmail.com>
2014-11-19 15:57:38 -0800

    added printing

--- pa6_explore.py
+++ pa6_explore.py
@@ -1,3 +1,5 @@
+import math as math
+
 #Exploring Python's generators for Project 6
 
 ###Simplest generator examples
@@ -15,7 +17,7 @@ for value in simple_generator_function():
 # 2
 # 3
 
-our_generator = simple_generator_funciton()
+our_generator = simple_generator_function()
 
 next(our_generator)
 #Should ouput:
@@ -65,6 +67,8 @@ def firstn(n):
 
 sum_of_first_n = sum(firstn(10000))
 
+print "sum_of_first_n " + str(sum_of_first_n)
+
 ###Cannonical examples:
 
 #We can rewrite this non-working solution:
@@ -85,6 +89,18 @@ def solve_number_10():
 
 #with generator functions as follows:
 
+def is_prime(number):
+    if number > 1:
+        if number == 2:
+            return True
+        if number % 2 == 0:
+            return False
+        for current in range(3, int(math.sqrt(number) + 1), 2):
+            if number % current == 0: 
+                return False
+        return True
+    return False
+
 def get_primes(number):
   while True:
     if is_prime(number):
@@ -92,30 +108,47 @@ def get_primes(number):
     number += 1
 
 def solve_number_10():
-
     total = 2
     for next_prime in get_primes(3):
-        if next_prime < 2000000:
+        if next_prime < 2000:
             total += next_prime
         else:
-            print(total)
+            print("Sum of primes up until 2000 " + str(total))
             return
+
+solve_number_10()
 
 #RangeGenerator:
 total = 0
-for i in irange(1000000):
-	total += 1
+for i in range(1000000):
+  total += 1
+
+print "range generator " + str(total)
 
 #Composing a generator:
-square = (i*i for i in irange(100000))
+square = (i*i for i in range(100000))
 total = 0
 for i in square:
-	total += i
+  total += i
+
+
+print "composing a generator " + str(total)
 
 ###Passing values into generators:
 
-def print_successive_primes(iterators, base=10):
+def get_primes(number):
+    while True:
+        if is_prime(number):
+            number = yield number
+        number += 1
+
+
+print()
+print("printing successive primes by passing values into generators")
+def print_successive_primes(iterations, base=10):
   prime_generator = get_primes(base)
   prime_generator.send(None)
   for power in range(iterations):
-		print(prime_generator.send(base ** power))
+    print(prime_generator.send(base ** power))
+
+print_successive_primes(10)
