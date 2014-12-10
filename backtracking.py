2ef0dfbfea08d00f620096a075e73954e4c24b53
rickbhardwaj <rickbhardwaj@gmail.com>
2014-12-10 02:57:44 -0800

    Converted Python backtracking pseudocode to valid generators.

---
+++ backtracking.py
@@ -0,0 +1,34 @@
+  # For the regex (abc|de)x
+  patt = seq(alt(prim("abc"), prim("de")), prim("x"))
+  match(s, patt)
+
+  def match(s, patt):
+    matches = patt(s, 1)
+    for pos in matches:
+      if pos == len(s)+1:
+        return True
+    return False
+
+  def prim(st):
+    def prim_helper(s, pos):
+      if pos+len(st) < len(s) and s[pos:pos+len(st)] == st:
+        yield pos + len(st)
+
+    return lambda s, pos: prim_help(s, pos)
+ 
+#seq(prim("h"), prim("ello")) 
+  def seq(patt1, patt2):
+    def seq_helper(s, pos):
+      for npos in patt1(s, pos):
+        patt2(s, npos)
+    return lambda s, pos: seq_helper(s, pos)
+
+  
+  def alt(patt1, patt2):
+    def alt_helper(s, pos):
+      patt1(s, pos)
+      patt2(s, pos)
+    return lambda s, pos: alt_helper(s, pos)
+
+
+seq(prim("abc"), prim("def"))
