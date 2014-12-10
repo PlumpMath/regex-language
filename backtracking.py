2e4d2ac71c910a08d94b3669fb03308c644dc024
rickbhardwaj <rickbhardwaj@gmail.com>
2014-12-10 14:16:18 -0800

    Fixed yielding to sequence

--- backtracking.py
+++ backtracking.py
@@ -1,15 +1,18 @@
 # For the regex (abc|de)x
 
 def match(s, patt):
-    matches = patt(s, 1)
+    matches = patt(s, 0)
     for pos in matches:
-        if pos == len(s)+1:
+        print(pos)
+        if pos == len(s):
+            print True
             return True
+        print False
         return False
 
 def prim(st):
     def prim_helper(s, pos):
-        if pos+len(st) < len(s) and s[pos:pos+len(st)] == st:
+        if pos+len(st) <= len(s) and s[pos:pos+len(st)] == st:
             yield pos + len(st)
 
     return lambda s, pos: prim_helper(s, pos)
@@ -18,7 +21,8 @@ def prim(st):
 def seq(patt1, patt2):
     def seq_helper(s, pos):
         for npos in patt1(s, pos):
-            patt2(s, npos)
+            for mpos in patt2(s, npos):
+                yield mpos
     return lambda s, pos: seq_helper(s, pos)
 
 
@@ -28,7 +32,5 @@ def alt(patt1, patt2):
         patt2(s, pos)
     return lambda s, pos: alt_helper(s, pos)
 
-
-patt = seq(prim("abc"), prim("def"))
-#patt = seq(alt(prim("abc"), prim("de")), prim("x"))
-match("def", patt)
+patt = seq(prim("his"), prim("tory"))
+match("history", patt)
