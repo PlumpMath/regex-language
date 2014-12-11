9acde10894a6c2872a296f2878ce70b86eb1e815
rickbhardwaj <rickbhardwaj@gmail.com>
2014-12-10 17:41:13 -0800

    Parsing for parenthesis, alts, nested alts complete

---
+++ parser.py
@@ -0,0 +1,61 @@
+class Node(object):
+    def __init__(self):
+        self.regex = None
+	self.prim_value = None
+        self.left = None
+	self.right = None
+
+def parse(tokens):
+    node = Node()
+    #seq = tokens.find(")")
+    paren_counter = [0, False]
+    seq = 0
+    for token in tokens:
+	if(token == "("):
+	    paren_counter[0] += 1 	
+	    paren_counter[1] = True	
+
+	if(token == ")"):
+	    paren_counter[0] -= 1
+	
+	if(paren_counter[0] == 0 and paren_counter[1] == True):
+	    break
+	seq += 1
+
+    def process(s):
+	if s.count("(") > s.count(")"):
+	   s = s[1:]
+	if s.count("(") < s.count(")"):
+	   s = s[:-1]
+	return s
+
+    if not "(" in tokens and not ")" in tokens and not "|" in tokens:
+	node.regex = "prim"
+	node.prim_value = tokens
+
+    if seq:
+	if seq == len(tokens) - 1:
+	    node = parse(tokens[1:-1])
+	if seq < len(tokens) - 1:
+	    if tokens[seq+1] == "(": 
+		node.regex = "seq"
+		node.left = parse(tokens[:seq+1])
+		node.right = parse(tokens[seq+1:])
+	    if tokens[seq+1] == "|": 
+		node.regex = "alt"
+		print("first half " + tokens[:seq+1])
+		node.left = parse(process(tokens[:seq+1]))
+		print("second half " + tokens[seq+2:])
+		node.right = parse(process(tokens[seq+2:]))
+	
+    return node
+
+inp = "((a)|(b))|(c)"
+ast = parse(inp)
+print(ast.regex)
+print(ast.left.regex)
+print(ast.left.left.regex)
+print(ast.left.right.regex)
+print(ast.right.regex)
+
+
