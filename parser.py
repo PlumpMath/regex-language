d366e5b50de79709ca394f770366746f440786ca
Puneet Dewan <dewan.puneet@gmail.com>
2014-12-12 14:34:59 -0800

    kleene and plus added to parser

--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@ class Node(object):
 	self.prim_value = None
         self.left = None
 	self.right = None
+        self.child = None
 
 def parse(tokens):
     node = Node()
@@ -22,16 +23,24 @@ def parse(tokens):
 	    break
 	seq += 1
 
-    def process(s):
-	if s.count("(") > s.count(")"):
-	   s = s[1:]
-	if s.count("(") < s.count(")"):
-	   s = s[:-1]
-	return s
+#    def process(s):
+#	if s.count("(") > s.count(")"):
+#	   s = s[1:]
+#	if s.count("(") < s.count(")"):
+#	   s = s[:-1]
+#	return s
+
 
     if not "(" in tokens and not ")" in tokens and not "|" in tokens:
-	node.regex = "prim"
-	node.prim_value = tokens
+        if "*" in tokens:
+            node.regex = "kleene"
+            node.child = parse(tokens[:-1])
+        elif "+" in tokens:
+            node.regex = "plus"
+            node.child = parse(tokens[:-1])
+        else:
+            node.regex = "prim"
+	    node.prim_value = tokens
 
     if seq:
 	if seq == len(tokens) - 1:
@@ -44,18 +53,16 @@ def parse(tokens):
 	    if tokens[seq+1] == "|": 
 		node.regex = "alt"
 		print("first half " + tokens[:seq+1])
-		node.left = parse(process(tokens[:seq+1]))
+		node.left = parse(tokens[:seq+1])
 		print("second half " + tokens[seq+2:])
-		node.right = parse(process(tokens[seq+2:]))
+		node.right = parse(tokens[seq+2:])
 	
     return node
 
-inp = "((a)|(b))|(c)"
+inp = "((a)|(c*))|((b))"
 ast = parse(inp)
 print(ast.regex)
 print(ast.left.regex)
 print(ast.left.left.regex)
 print(ast.left.right.regex)
 print(ast.right.regex)
-
-
