b89b2720c83564691a264a64b1833c7809b57afc
rickbhardwaj <rickbhardwaj@gmail.com>
2014-12-15 17:16:24 -0800

    Added cool test cases

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from backtracking import *
 
 print("Matching bc with the regex a|bc returns..." + str(regex_matching("bc", "a|bc")))
 print("\n")
+print("Matching aaaaa with the regex (a*)?aaaaa returns..." + str(regex_matching("bc", "a|bc")))
+print("\n")
 print("Matching aab with the regex aab|a?aab returns..." + str(regex_matching("aab", "a?aab")))
 print("\n")
 print("Matching aaaaaaba with the regex (aa)*aaba returns..." + str(regex_matching("aaaaaaba", "(aa)*aaba")))
@@ -15,6 +17,10 @@ print("Matching aabadddehijk with the regex aaba(ad)*|hi|(d*e)|g(hi)jk returns..
 print("\n")
 print("Matching add with the regex (ad)*d* returns..." + str(regex_matching("add", "(ad)*d*")))
 print("\n")
+print("Matching xxxxxxxxxxxxxxxxxxxxxxxxxxxxy with the regex (x+x+)+y returns..." + str(regex_matching("xxxxxxxxxxxxxxxxxxxxxxxxxxxxy", "(x+x+)+y")))
+print("\n")
+print("Matching xxxxxxxxxxxxxxxxxxxxxxxxxxxx with the regex (x+x+)+y returns..." + str(regex_matching("xxxxxxxxxxxxxxxxxxxxxxxxxxxx", "(x+x+)+y")))
+print("\n")
 
 roster = ["rick", "puneet", "bodik", "aaron"]
 for i in roster:
@@ -24,14 +30,20 @@ for i in roster:
 	print(str(i) + " is a student")
 
 print("\n")
-#Valid regex, also demonstrates potential for desugaring other regexs
-any_letter = "(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z)*"
-email_regex = any_letter + "_?" + any_letter + "@" + any_letter + "." + "(com)|(edu)|(net)"
 
-emails = ["rickbhardwaj@gmail.com", "bodik@berkeley.edu", "malware@ch.cm"]
+#Valid regex, also demonstrates potential for desugaring other regexs in our DSL
+
+any_letter = "(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z)*"
+any_number = "(0|1|2|3|4|5|6|7|8|9)*"
+email_regex = any_letter + "_?" + any_letter + any_number +"@" + any_letter + "." + "(com)|(edu)|(net)"
+
+print("Email Regex: " + str(email_regex))
+
+emails = ["rickbhardwaj1993@gmail.com", "bodik@berkeley.edu", "malware@ch.cm"]
 
 for i in emails:
     if regex_matching(i, email_regex):
 	print(str(i) + " is a valid email")
     else:
 	print(str(i) + " is NOT a valid email")
+print("\n")
